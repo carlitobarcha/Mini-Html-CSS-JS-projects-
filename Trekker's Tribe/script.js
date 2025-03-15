6bbbeae255a27062d2631b836e9bc6ad77a83023
Khalid Abbas <159667330+carlitobarcha@users.noreply.github.com>
2025-03-16 00:53:43 +0500

    Add files via upload

---
+++ Trekker's Tribe/script.js
@@ -0,0 +1,11 @@
+// Booking Form Submission
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for booking with Trekkers Tribe! We will contact you soon.');
+});
+
+// Contact Form Submission
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting Trekkers Tribe! We will get back to you soon.');
+});
