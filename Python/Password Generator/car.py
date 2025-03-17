872d762862d97460b20a8604968631d01861a67a
Khalid Abbas <159667330+carlitobarcha@users.noreply.github.com>
2025-03-17 23:50:10 +0500

    Add files via upload

---
+++ Python/Password Generator/car.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Generate a random password of length 12
+print("Generated Password:", generate_password())
